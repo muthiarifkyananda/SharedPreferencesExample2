a11588e73b2c67d412cae67aee2dd227c62bdb7e
muthiarifkyananda <muthiarifkyananda@gmail.com>
2024-11-10 19:41:22 +0700

    push.

---
+++ app/src/main/java/muthiaraa/sharedpreferencesexample/SecondActivity.kt
@@ -0,0 +1,34 @@
+package muthiaraa.sharedpreferencesexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import muthiaraa.sharedpreferencesexample.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLoad.setOnClickListener {
+            val filename = "$packageName-${MainActivity.RPL}"
+            val pref = getSharedPreferences(filename, Context.MODE_PRIVATE)
+            val firstName = pref.getString("firstName", "")
+            val lastName = pref.getString("lastName", "")
+            binding.tvOutput.text = "$firstName $lastName "
+        }
+
+    }
+    class MainActivity : AppCompatActivity() {
+        companion object {
+            const val RPL = "preferences"
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        binding.tvOutput.text = "Click Button Load Data"
+    }
+}
